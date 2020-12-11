e158c42b3f99217d80e5c906e2ab999b77cb8bd1
abdelmawlaSouat <abdelmawla.souat@gmail.com>
2020-12-11 18:53:00 +0100

    Remove Grid in Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,20 +7,18 @@
  * Copyright (c) 2020 Shuriken
  */
 
-import { Grid } from '@material-ui/core'
+import { Box } from '@material-ui/core'
 import Sources from './Sources'
 
 function Footer() {
 	return (
-		<Grid container>
-			<Grid item>
-				<Sources
-					author="Abdelmawla Souat"
-					authorProfileLink="https://www.linkedin.com/in/abdelmawla-souat/"
-					challengeLink="https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW"
-				/>
-			</Grid>
-		</Grid>
+		<Box display="flex" justifyContent="center">
+			<Sources
+				author="Abdelmawla Souat"
+				authorProfileLink="https://www.linkedin.com/in/abdelmawla-souat/"
+				challengeLink="https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW"
+			/>
+		</Box>
 	)
 }
 
