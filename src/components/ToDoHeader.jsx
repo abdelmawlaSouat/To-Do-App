c8db42648e4653fc5b5b124e6b739bfae4030df0
abdelmawlaSouat <abdelmawla.souat@gmail.com>
2020-12-12 10:07:16 +0100

    Add grid to ToDoHeader component

--- src/components/ToDoHeader.jsx
+++ src/components/ToDoHeader.jsx
@@ -7,21 +7,25 @@
  * Copyright (c) 2020 Shuriken
  */
 
-import { Box, IconButton, Typography } from '@material-ui/core'
+import { Box, Grid, IconButton, Typography } from '@material-ui/core'
 import Proptypes from 'prop-types'
 
 function ToDoHeader({ title, themeIcon, handleDarkTheme }) {
 	return (
-		<Box
-			className="ToDoApp-header"
-			display="flex"
-			justifyContent="space-between"
-		>
-			<Typography variant="h4">{title}</Typography>
-			<IconButton onClick={() => handleDarkTheme()}>
-				<img src={themeIcon} alt="Theme Icon" />
-			</IconButton>
-		</Box>
+		<Grid container justify="center" className="ToDoApp-header">
+			<Grid item md={5}>
+				<Box
+					display="flex"
+					justifyContent="space-between"
+					alignItems="center"
+				>
+					<Typography variant="h4">{title}</Typography>
+					<IconButton onClick={() => handleDarkTheme()}>
+						<img src={themeIcon} alt="Theme Icon" />
+					</IconButton>
+				</Box>
+			</Grid>
+		</Grid>
 	)
 }
 
